d71fb9f39a655223309036d2d76bc25e6f04449b
hayek2024 <githubaprendizaje@zapotecorum.org>
2025-08-29 07:19:33 -0600

    Fix: Add missing user parameter to posts.index redirect in RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         auth()->attempt($request->only('email', 'password'));
 
         // Redireccionar
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', auth()->user()->username);
     
     }
 
